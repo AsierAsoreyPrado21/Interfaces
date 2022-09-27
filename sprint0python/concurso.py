4829f1bf513c4852d18124dcbb6abd7861f7aa26
Asier Asorey Prado <aasoreyp@fpcoruna.afundacion.org>
2022-09-27 09:23:38 +0200

    Ejercicio 1 completado

---
+++ sprint0python/concurso.py
@@ -0,0 +1,9 @@
+print ("Que día es hoy?")
+print ("a:Lunes")
+print ("b:Martes")
+print ("c:Miercoles")
+opcion= input('Respuesta: ')
+if opcion=="b":
+	print ("Respuesta correcta")
+else:
+	print ("Respuesta incorrecta")
