4318e125fc3cf024a96a09b75e4f5a6181f30b57
Asier Asorey Prado <aasoreyp@fpcoruna.afundacion.org>
2022-09-28 10:17:42 +0200

    Tarea 6 completada

--- sprint0python/principal.py
+++ sprint0python/principal.py
@@ -1,14 +1,26 @@
 from fibonacci import Fibonacci
 from fibonacci2 import Fibonacci2
+import time
+op = input('Tu respuesta: ')
 while True:
-    op = input('Tu respuesta: ')
+
     if op == "a":
         number = int(input("Valor de n (numero de veces que realiza la funcion): "))
         print("Secuencia Fibonacci :"+str (Fibonacci(number)))
+        start_time = time.time()
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print('El tiempo de ejecución ha sido :' + str(elapsed_time) + ' s')
         break
     elif op == "b":
         number = int(input("Valor de n (numero de veces que realiza la funcion): "))
         print("Secuencia Fibonacci :"+str (Fibonacci2(number)))
+        start_time = time.time()
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print('El tiempo de ejecución ha sido :' + str(elapsed_time) + ' s')
         break
     else:
          op = input('Tu respuesta. Debe ser a o b: ')
+
+
