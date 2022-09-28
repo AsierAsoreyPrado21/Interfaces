ce4771175275837f909118a76a99a85f6c182088
Asier Asorey Prado <aasoreyp@fpcoruna.afundacion.org>
2022-09-28 10:03:07 +0200

    Tarea 5 completada

--- sprint0python/principal.py
+++ sprint0python/principal.py
@@ -1,8 +1,14 @@
-
 from fibonacci import Fibonacci
 from fibonacci2 import Fibonacci2
-number = int(input("Valor de n (numero de veces que realiza la funcion): "))
-print("Secuencia Fibonacci :"+str (Fibonacci(number)))
-
-number = int(input("Valor de n (numero de veces que realiza la funcion): "))
-print("Secuencia Fibonacci :"+str (Fibonacci2(number)))
+while True:
+    op = input('Tu respuesta: ')
+    if op == "a":
+        number = int(input("Valor de n (numero de veces que realiza la funcion): "))
+        print("Secuencia Fibonacci :"+str (Fibonacci(number)))
+        break
+    elif op == "b":
+        number = int(input("Valor de n (numero de veces que realiza la funcion): "))
+        print("Secuencia Fibonacci :"+str (Fibonacci2(number)))
+        break
+    else:
+         op = input('Tu respuesta. Debe ser a o b: ')
