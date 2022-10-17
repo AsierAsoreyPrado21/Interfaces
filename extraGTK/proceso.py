d74ad83e8d404cd2febb41a13ecb7695d415b15d
Asier Asorey Prado <aasoreyp@fpcoruna.afundacion.org>
2022-10-17 11:27:11 +0200

    Fotos API-REST

---
+++ extraGTK/proceso.py
@@ -0,0 +1,13 @@
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+class Detail(Gtk.Window):
+
+    def __init__(self, title, image, description):
+        super().__init__(title=title)
+        box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        box.pack_start(image, False, False, 0)
+        box.pack_start(Gtk.Label(label=description), True, True, 0)
+        self.add(box)
+        self.connect("destroy", Gtk.main_quit)
