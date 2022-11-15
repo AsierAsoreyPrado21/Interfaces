c5cdf0c78b7ee5b13d1cffd63a9e7cfe36373bc9
Asier Asorey Prado <aasoreyp@fpcoruna.afundacion.org>
2022-11-15 09:13:51 +0100

    Tarea 5 completada

---
+++ sprint3android/app/src/main/java/com/example/catalogactivity/CatalogFragment.java
@@ -0,0 +1,31 @@
+package com.example.catalogactivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class CatalogFragment extends Fragment {
+    private Button button;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View view = inflater.inflate(R.layout.activity_catalog, container, false);
+        Context context = view.getContext();
+        button = view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, DetailActivity.class);
+                context.startActivity(intent);
+            }
+        });
+        return view;
+    }
+}
