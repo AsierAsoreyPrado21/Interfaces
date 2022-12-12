0f38fa406d1cec6fadcecf1e6497cfded83c5c6d
Asier Asorey Prado <aasoreyp@fpcoruna.afundacion.org>
2022-12-12 17:44:13 +0100

    Casi terminado

--- calculadoraAndroid/app/src/main/java/com/example/calculadora/MainActivity.java
+++ calculadoraAndroid/app/src/main/java/com/example/calculadora/MainActivity.java
@@ -2,13 +2,83 @@ package com.example.calculadora;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+private TextView Screen;
+private Button AC,Power,Back,Div,Mul,Plus,Min,One,Two,Three,Four,Five,Six,Seven,Eight,Nine,Zero,Ans,Point,Equals;
+private String input,Answer;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Screen=findViewById(R.id.screen);
+        AC=findViewById(R.id.ac);
+        Power=findViewById(R.id.power);
+        Back=findViewById(R.id.back);
+        Div=findViewById(R.id.div);
+        Seven=findViewById(R.id.seven);
+        Eight=findViewById(R.id.eight);
+        Nine=findViewById(R.id.nine);
+        Mul=findViewById(R.id.mul);
+        Four=findViewById(R.id.four);
+        Five=findViewById(R.id.five);
+        Six=findViewById(R.id.six);
+        One=findViewById(R.id.one);
+        Two=findViewById(R.id.two);
+        Three=findViewById(R.id.three);
+        Min=findViewById(R.id.min);
+        Plus=findViewById(R.id.plus);
+        Zero=findViewById(R.id.zero);
+        Point=findViewById(R.id.point);
+        Ans=findViewById(R.id.ans);
+        Equals=findViewById(R.id.equals);
+
     }
+    public void ButtonClick(View view){
+        Button button = (Button) view;
+        String data=button.getText().toString();
+        switch(data){
+            case"AC":
+                input="";
+                break;
+            case"Ans":
+                input+=Answer;
+                break;
+            case"*":
+                Solve();
+                input+="*";
+                break;
+            case"^":
+                Solve();
+                input+="^";
+                break;
+            case"=":
+                Solve();
+                Answer=input;
+                break;
+            case"DEL":
+                String newText=input.substring(0,input.length()-1);
+                input=newText;
+                break;
+            default:
+                if(input==null){
+                    input="";
+                }
+                if(data.equals("+")||data.equals("-")||data.equals("/")){
+                    Solve();
+                }
+                input+=data;
+        }
+        Screen.setText(input);
+    }
+    private void Solve() {
+
+    }
+
 }
